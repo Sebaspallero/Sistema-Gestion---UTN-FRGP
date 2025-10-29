b79d846356f8c11b3ad34dcc9c5bddb41a9a3242
carrizoberterodev <carrizobertero.dev@gmail.com>
2025-10-29 16:15:38 -0300

    Actualizacion
    Actualizacion de las entidades Categoria y Persona

--- Entidades/Categoria.cpp
+++ Entidades/Categoria.cpp
@@ -2,9 +2,9 @@
 #include "../EntidadesH/Categoria.h"
 #include <cstring>
 
-int Categoria::getIDCategoria() const{};
+int Categoria::getIDCategoria() const{return _idCategoria;};
 std::string Categoria::getNombre() const{return std::string(_nombre);};
-bool Categoria::getEstado() const{};
+bool Categoria::getEstado() const{return _estado;};
 void Categoria::setIDCategoria(int idCategoria){}
 void Categoria::setNombre(std::string& nombre){
 strncpy(_nombre,nombre.c_str(),sizeof(_nombre)-1);
--- Entidades/Persona.cpp
+++ Entidades/Persona.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "../EntidadesH/Persona.h"
 
-int Persona::getID() const{};
-std::string Persona::getNombre() const{};
-std::string Persona::getApellido() const{};
-Fecha Persona::getFechaNacimiento() const{};
-int Persona::getDNI() const{};
-std::string Persona::getEmail() const{};
-bool Persona::getEstado() const{};
+int Persona::getID() const{return _id;};
+std::string Persona::getNombre() const{return std::string(_nombre);};
+std::string Persona::getApellido() const{return std::string(_apellido);};
+Fecha Persona::getFechaNacimiento() const{return _fechaNacimiento;};
+int Persona::getDNI() const{return _dni;};
+std::string Persona::getEmail() const{return std::string(_email);};
+bool Persona::getEstado() const{return _estado;};
 void Persona::setID(int id){};
 void Persona::setNombre(std::string& nombre){};
 void Persona::setApellido(std::string& apellido){};
